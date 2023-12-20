4aafc971ac05e1a345d09444d8a879dac26adf1d
Leopold <40013973+Roguehp98@users.noreply.github.com>
2023-12-20 22:15:40 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 // Serve API requests from the router
 app.use('/api', router);
 
-console.log('abc')
+console.log('abc 123')
 
 // Serve storybook production bundle
 app.use('/storybook', express.static('dist/storybook'));
